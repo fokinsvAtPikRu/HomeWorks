ca6c68a0b1e4de166fb5ae185eae243f22a502cd
Фокин Сергей <fokinsv@pik.ru>
2025-02-20 11:40:07 +0300

    Добавлены вызовы для 1 и 2 задания

--- Lesson2_Task1/Lesson2.cs
+++ Lesson2_Task1/Lesson2.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
+﻿using System.Numerics;
 using static System.Math;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson2
 {
--- Lesson2_Task1/Program.cs
+++ Lesson2_Task1/Program.cs
@@ -16,7 +16,7 @@ namespace Lesson2
                 Console.WriteLine("5. Округление вверх.");
                 Console.WriteLine("6. Округление до 0,5.");
                 Console.WriteLine("7. Поменть значение двух переменных без третьей.");
-                Console.WriteLine("Еsc - выход");
+                Console.WriteLine("0 - выход");
                 Console.Write("Введите число:");
                 var str=Console.ReadLine();
                 try
@@ -31,38 +31,72 @@ namespace Lesson2
                 Console.WriteLine("Для продолжения нажмите любую кнопку. Для выхода ESC.");
                 key = Console.ReadKey().Key;
                 Console.Write(((int)key));
+                Console.Clear();
             }
 
 
         }
         public static void ChoiceTask(int choice) 
         {
-        switch(choice)
+            Console.Clear();
+            switch(choice)
             {
                 case 1:
+                    Task1();
+                    break;
+                case 2:
+                    Task2();
+                    break;
+                case 3:
 
                     break;
-                    case 2:
+                case 4:
 
                     break;
-                    case 3:
+                case 5:
 
                     break;
-                    case 4:
+                case 6:
 
                     break;
-                    case 5:
-
-                    break;
-                    case 6:
-
-                    break;
-                    case 7:
+                case 7:
 
                     break;
             }
         }
+        public static void  Task1()
+        {
+            Console.Write("Введите первое значение:");
+            var firstArg= Console.ReadLine();
+            Console.Write("Введите второе значение:");
+            var secondArg = Console.ReadLine();
+            Console.Write("Введите третье значение:");
+            var thirdArg = Console.ReadLine();
+            Lesson2.SwapThreeVariables(ref firstArg,ref secondArg,ref thirdArg);
+            Console.WriteLine($"Значения после сдвига: {firstArg}, {secondArg}, {thirdArg}");
+        }
+        public static void Task2()
+        {
+            Console.Write("Градусы:");
+            // тут и дальше методы Convert надо обернуть в try catch
+            var angleDegrees = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Угловые минуты:");
+            var angleMinutes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Угловые секунды:");
+            var angleSeconds = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Величина угла в радианах: {Lesson2.AngleFromDegreesToRadians(angleDegrees, angleMinutes, angleSeconds)}");
+        }
+        public static void Task3()
+        {
+            Console.Write("Градусы:");
+            // тут и дальше методы Convert надо обернуть в try catch
+            var angleDegrees = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Угловые минуты:");
+            var angleMinutes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Угловые секунды:");
+            var angleSeconds = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Величина угла в радианах: {Lesson2.AngleFromDegreesToRadians(angleDegrees, angleMinutes, angleSeconds)}");
+        }
 
-            
     }
 }
