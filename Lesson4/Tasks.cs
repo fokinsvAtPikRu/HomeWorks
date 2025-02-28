c42eb16edb9c0965ef1ce9bb772b7d27b73b4010
Фокин Сергей <fokinsv@pik.ru>
2025-02-28 11:28:15 +0300

    Урок 4. Добавлено 1 задание. Рекурсивный факториал

---
+++ Lesson4/Tasks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public static class Tasks
+    {
+        /// <summary>
+        /// Task 1. Factorial recursion
+        /// </summary>
+        /// <returns></returns>
+        public static int FactorialRecursion(int n)
+        {
+            int factorial;
+            if (n == 1) return 1;
+            else
+            {
+                factorial=n*FactorialRecursion(--n);
+            }
+            return 0;
+        }
+    }
+}
---
+++ Lesson4Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Lesson4
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void FactorialRecursion_SomeData_Result()
+        {
+
+        }
+    }
+}
