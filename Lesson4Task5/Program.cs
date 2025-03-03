5e351373f4d400e81e630e6e9fab67de2422130d
Фокин Сергей <fokinsv@pik.ru>
2025-03-03 12:21:11 +0300

    Lesson 4 is complete

--- Lesson4Task5/Program.cs
+++ Lesson4Task5/Program.cs
@@ -7,30 +7,30 @@ class Lesson4Task5
         int max = 60;
         while (true)
         {
-            Console.WriteLine($"Введите число от {min} до {max} включительно:");
+            Console.Write($"Введите число от {min} до {max} включительно:");
             string str = Console.ReadLine();
             int? result=ConvertStringToInt(str);
-            if (result != null && IsValueInRange(result, min, max))
+            if (result is null)
             {
-                Console.WriteLine($"{result} корректное значение.");
+                Console.WriteLine($"{str} не является числом.");
+                Console.ReadKey();
+            }
+            else if (!IsValueInRange(result, min, max))
+            {
+                Console.WriteLine($"{result} вне диапазона [{min},{max}].");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"{result} является корректным значением.\nПрограмма завершена.");
+                Console.ReadKey();
                 break;
             }
             Console.Clear();
         }
 
     }
-    private static int? ConvertStringToInt(string str)
-    {
-        if (Int32.TryParse(str, out int value))
-        {
-            return value;
-        }
-        else
-        {
-            Console.WriteLine($"{str} не является числом.");
-            return null;
-        }
-    }
+    private static int? ConvertStringToInt(string str) => Int32.TryParse(str, out int value) ? value : null;    
     private static bool IsValueInRange(int? value, int min, int max) => value >= min && value <= max;
 
 }
