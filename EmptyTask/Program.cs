2230b6d84b18da7db0096fe9a19528e34dda3653
Фокин Сергей <fokinsv@pik.ru>
2025-03-09 12:12:00 +0400

    Added tests for task1

--- EmptyTask/Program.cs
+++ EmptyTask/Program.cs
@@ -1,17 +1,35 @@
 ﻿using System.Text;
 
 public class Program
-{    public static void Main(string[] args)
+{
+    public enum EvenAndOddElements // для результата первого задания
     {
-        int i=ReverseDigitHelper(87, 0);
-        i = i + 1;
+        EvenIsMore,
+        OddIsMore,
+        Equal
     }
-    private static int ReverseDigitHelper(int digit, int digitReverse)
+    public static void Main(string[] args)
     {
-        if (digit == 0)
-            return digitReverse;
-        else
-            return ReverseDigitHelper(digit / 10, digitReverse * 10 + digit % 10);
+        IsNumberOfEvenElementsGreater(new int[] { 1, 2, 3 });
     }
+    public static int FoldArrayValue(int[] array, int acc, Func<int, int, bool> f)              // свертка массива foreach
+    {                                                                                          // для задач где не нужен  
+        foreach (int element in array)                                                         // индекс элемента  
+            if (f(element, acc)) acc++;
+        return acc;
+    }
+    public static int CountOfEvenElements(int[] array) =>
+            FoldArrayValue(array, 0, (element, acc) => element % 2 == 0 ? true : false);              // количество четных элементов
 
+    public static int CountOfOddElements(int[] array) =>
+            FoldArrayValue(array, 0, (element, acc) => element % 2 != 0 ? true : false);              // количество нечетных элементов
+    public static EvenAndOddElements IsNumberOfEvenElementsGreater(int[] array)                      // метод выполняет 1 задание урока 5
+    {
+        int even = CountOfEvenElements(array);
+        int odd = CountOfOddElements(array);
+        return
+            even > odd ? EvenAndOddElements.EvenIsMore :    // больше четных
+            even < odd ? EvenAndOddElements.OddIsMore :     // больше нечетных 
+            EvenAndOddElements.Equal;                                                                     // равное количество            
+    }
 }
---
+++ Lesson5/Lesson5.cs
@@ -0,0 +1,71 @@
+﻿using Lesson3;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lesson5
+{
+    public enum EvenAndOddElements // для результата первого задания
+    {
+        EvenIsMore,
+        OddIsMore,
+        Equal
+    }
+    public class Lesson5
+    {
+        public static int[] CreateArray(int length) => new int[length];               // фабрика массивов
+        public static void AddRandomElements(int[] array, int min, int max)           // заполнение массива случайными значениями 
+        {
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(min, max);
+        }
+        public static int FoldArrayValue(int[] array, int acc, Func<int, int, int> f)             // свертка массива foreach
+        {                                                                                         // для задач где не нужен              
+            foreach (int element in array)                                                        // индекс элемента  
+                acc = f(element, acc);
+            return acc;
+        }
+        public static int FoldArrayIndex(int[] array, int accIndex, Func<int, int, int> f)       // свертка для задач где нужен индекс
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                accIndex = f(array[i], accIndex);
+            }
+            return accIndex;
+        }
+        public static int CountOfEvenElements(int[] array) =>
+            FoldArrayValue(array, 0, (element, acc) => element % 2 == 0 ? ++acc : acc);              // количество четных элементов
+        public static int CountOfOddElements(int[] array) =>
+            FoldArrayValue(array, 0, (element, acc) => element % 2 != 0 ? ++acc : acc);              // количество нечетных элементов
+        public static int CountOfPositiveElements(int[] array) =>
+            FoldArrayValue(array, 0, (element, acc) => element > 0 ? ++acc : acc);                   // количество положительных чисел
+
+        public static int CountOfNegativeElements(int[] array) =>
+            FoldArrayValue(array, 0, (element, acc) => element < 0 ? ++acc : acc);                   // количество отрицательных чисел
+
+        public static int CountOfZeroElements(int[] array) =>
+            FoldArrayValue(array, 0, (element, acc) => element == 0 ? ++acc : acc);                  // количество нулей
+        // 
+        public static int MaxElementIndex(int[] array) =>                                            // индекс макс элемента
+            FoldArrayIndex(array, 0,
+                (elementIndex, maxIndex) =>
+                array[elementIndex] > array[maxIndex] ? maxIndex = elementIndex : maxIndex);
+
+
+        public static EvenAndOddElements IsNumberOfEvenElementsGreater(int[] array)  // метод выполняет 1 задание урока 5
+        {
+            int even = CountOfEvenElements(array);
+            int odd = CountOfOddElements(array);
+            return
+                even > odd ? EvenAndOddElements.EvenIsMore :    // больше четных
+                even < odd ? EvenAndOddElements.OddIsMore :     // больше нечетных 
+                EvenAndOddElements.Equal;                                                                     // равное количество            
+        }
+        public static (int, int, int) CountPositiveNegativeZeroElements(int[] array) =>
+            (CountOfPositiveElements(array), CountOfNegativeElements(array), CountOfZeroElements(array));    // метод выполняет 2 задание урока 5
+
+
+
+    }
+}
