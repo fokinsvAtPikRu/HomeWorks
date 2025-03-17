120d3a3afa38dbb5604726ce9734792ece276b1b
Фокин Сергей <fokinsv@pik.ru>
2025-03-17 07:30:16 +0400

    задание 1 и 2 готовы

---
+++ Lesson6/Lesson6.cs
@@ -0,0 +1,31 @@
+﻿namespace Lesson6
+{
+    public static class Lesson6
+    {
+        public static string Task1LongestWord(string phrase)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            var words=phrase.Split(delimiterChars);
+            string wordMax=String.Empty;
+            foreach (var word in words)
+            {
+                if (word.Length > wordMax.Length)
+                    wordMax=word;
+            }
+            return wordMax;
+        }
+        public static bool Task2IsPalindrom(string phrase)
+        {
+            string cleanedPhrase = phrase.Replace(" ", "").ToLower();            
+            for (int left = 0, right = cleanedPhrase.Length - 1; left < right; left++, right--)
+            {
+                if (cleanedPhrase[left] != cleanedPhrase[right])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Lesson6Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson6
+{
+    public class Lesson6Task1
+    {
+        [Theory(DisplayName ="Урок 6. Задача 1. Найти самое длинное слово.")]
+        [InlineData("As gsrep fawpokf sdfk df", "fawpokf")]
+        [InlineData("Сталин бывал в Можайске один раз, когда в 1945 году ехал поездом на Потсдамскую конференцию, где союзники делили мир.", "Потсдамскую")]
+        public void Test1(string phrase, string resultExpected)
+        {
+            // Arrange
+            
+            // Act
+            var result = Lesson6.Task1LongestWord(phrase);
+            // Assert
+            Assert.Equal(resultExpected, result);
+        }
+    }
+    public class Lesson6Task2Tests
+    {
+        [Theory(DisplayName = "Урок 6. Задача 2. Является ли фраза палиндромом.")]
+        [InlineData("А роза упала на лапу Азора", true)]
+        [InlineData("Сталин бывал в Можайске один раз, когда в 1945 году ехал поездом на Потсдамскую конференцию, где союзники делили мир.", false)]
+        public void Test1(string phrase, bool resultExpected)
+        {
+            // Arrange
+
+            // Act
+            var result = Lesson6.Task2IsPalindrom(phrase);
+            // Assert
+            Assert.Equal(resultExpected, result);
+        }
+    }
+}
