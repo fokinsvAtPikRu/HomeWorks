10c41d8678f3603b1836924a9f9d4b4fdfd1cbc5
Фокин Сергей <fokinsv@pik.ru>
2025-04-03 13:53:07 +0300

    задание 1 и 2 выполнено без тестов

---
+++ Lesson8/Lesson8Task1.cs
@@ -0,0 +1,35 @@
+﻿namespace Lesson8
+{
+    public class Lesson8Task1
+    {
+        public enum Operation
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+        public static double Calculate(double op1, double op2, Operation action)
+        {
+            double result = 0;
+            switch (action)
+            {
+                case Operation.Add:
+                    result = op1+op2;
+                    break;
+                    case Operation.Subtract:
+                    result = op1-op2;
+                    break;
+                    case Operation.Multiply:
+                    result = op1*op2;
+                    break;
+                    case Operation.Divide:
+                    if (op2 == 0)
+                        throw new DivideByZeroException("Деление на ноль!");
+                    result = op2/op1;
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ Lesson8/Lesson8Task2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public class Lesson8Task2
+    {
+        public static int ValidateAge(int age) =>
+            age < 0 ? throw new ArgumentException("Возраст не может быть отрицательным!") :
+                age > 150 ? throw new ArgumentOutOfRangeException("Слишком большой возраст") : age;
+    }
+}
