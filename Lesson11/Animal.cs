0e90a87dbe4b2d60d1234d73e6c490e6cb34f885
Фокин Серей <fokinsv@pik.ru>
2025-04-18 08:25:25 +0400

    задания к уроку 11 выполнены

---
+++ Lesson11/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    public abstract class Animal
+    {
+        public abstract string Name { get; }
+        public abstract string Say();
+        public string ShowInfo() => Name + " says  " + Say();
+    }
+}
