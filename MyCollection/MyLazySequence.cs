c7b66fbc8c33c74313250009578f191c861dd22f
Фокин Сергей <fokinsv@pik.ru>
2025-04-20 09:37:53 +0400

    бесконечная ленивая коллекция на yield return

---
+++ MyCollection/MyLazySequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection
+{
+    internal class MyLazySequence<T>:IEnumerable<T>
+    {
+        T _current;
+        Func<T, T> _iterator;
+        IEnumerator<T> _enumerator;
+        
+        public MyLazySequence(T current, Func<T, T> iterator)
+        {
+            _current = current;
+            _iterator = iterator;          
+            
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (true)
+            {
+                yield return _current;
+                _current = _iterator(_current);
+            }
+        }       
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
+
--- MyCollection/Program.cs
+++ MyCollection/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyCollection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
@@ -12,13 +13,15 @@ namespace MyEducation
         static void Main()
         {
             // Реализация с IEnumerable с yield return
-            //var collection = new MyCollection<int>(1, 2, 3);
-            //foreach (var item in collection)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            var collection = new MyCollection<int>(1, 2, 3);
+            Console.WriteLine("Моя коллекция");
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
 
             // Очередь на связанном списке
+            Console.WriteLine("\nОчередь на односвязаном списке");
             var queue = new MyQueue<int>();
             queue.Enqueue(1);
             queue.Enqueue(2);
@@ -27,6 +30,25 @@ namespace MyEducation
                 Console.WriteLine(item);
             while (!queue.IsEmpty)
                 Console.WriteLine(queue.Dequeue());
+
+
+            // возвращает 0,1,2,0,1,2,...
+            Func<int, int> iterator = (x) =>
+            {
+                x++;
+                return x % 3;
+            };
+            // создаем ленивую последовательность и печатаем 30 первых элементов
+            var sequence = new MyLazySequence<int>(0, iterator);
+            int i = 0;
+            Console.WriteLine("\nЛенивая коллекция");
+            foreach (var item in sequence)
+            {
+                Console.WriteLine(item);
+                i++;
+                if (i > 30)
+                    break;
+            }
         }
     }
 }
