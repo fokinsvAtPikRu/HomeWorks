5173fa08948adad3d5dc9784c0c647276e4b9648
Фокин Серей <fokinsv@pik.ru>
2025-06-13 10:12:52 +0400

    Задачи к 16 уроку выполнены

---
+++ Lesson16Task/ArrayTools.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16Task
+{
+    internal static class ArrayTools
+    {
+        public static int[] CreateArray(int count)
+        {
+            Console.WriteLine("Метод CreateArray запущен");
+            var rand=new Random();
+            var array = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = rand.Next(0, 100);
+                Console.WriteLine($"Элементу массива {i} присвоено значение:{array[i]}");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Метод CreateArray завершен");
+            return array;            
+        }
+        public static async Task<int[]> CreateArrayAsync(int count)
+        {
+            Console.WriteLine($"Метод CreateArrayAsync запущенн");
+            int[] array=await Task.Run(() => CreateArray(count));            
+            Console.WriteLine($"Метод CreateArrayAsync завершен");
+            return array;
+        }
+        public static int GetArraySumma(int[] array)
+        {
+            Console.WriteLine("Метод GetArraySumma запущен");           
+            int sum = 0;
+            foreach (var item in array)
+            {                
+                sum += item;
+                Console.WriteLine($"Элемент массива {item} обработан. Текущая суммма:{sum}");
+                Thread.Sleep(800);
+            }
+            Console.WriteLine("Метод GetArraySumma завершен");
+            return sum;
+        }
+        public static async Task<int> GetArraySummaAsync(int[] array)
+        {
+            Console.WriteLine($"Метод GetArraySummaAsync запущенн");
+            int sum = await Task.Run(() => GetArraySumma(array));
+            Console.WriteLine($"Метод GetArraySummaAsync завершен");
+            return sum;
+
+        }
+    }
+}
---
+++ Lesson16Task/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson16Task
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            Console.WriteLine("Задача продолжения");
+            Console.Write("Метод Main запущен");            
+            Task<int[]> task1 =new Task<int[]>(() => { return ArrayTools.CreateArray(5); });
+            task1.Start();
+            Task<int> task2 = task1.ContinueWith(t =>
+            {
+                int[] array=t.Result;
+                return ArrayTools.GetArraySumma(array);
+            });
+            Console.WriteLine($"Сумма массива равна {task2.Result}");
+            Console.Write("Метод Main завершен");
+            Console.WriteLine("Задача продолжения");
+            Console.Write("Метод Main запущен");
+            int[] array=await ArrayTools.CreateArrayAsync(5);
+            int sum = await ArrayTools.GetArraySummaAsync(array);
+            Console.WriteLine($"Сумма массива равна {sum}");
+            Console.Write("Метод Main завершен");
+        }
+    }
+}
